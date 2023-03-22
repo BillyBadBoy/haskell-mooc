a8be1ffd4e16430db71b76d2cc343ceee756e529
william Morgan <william.morgan@hotmail.co.uk>
2023-03-22 18:40:11 +0000

    wip

--- exercises/Set14b.hs
+++ exercises/Set14b.hs
@@ -73,12 +73,16 @@ getAllQuery = Query (T.pack "SELECT account, amount FROM events;")
 -- openDatabase should open an SQLite database using the given
 -- filename, run initQuery on it, and produce a database Connection.
 openDatabase :: String -> IO Connection
-openDatabase = todo
+openDatabase dbName = do
+  db <- open dbName
+  execute_ db initQuery
+  return db
 
 -- given a db connection, an account name, and an amount, deposit
 -- should add an (account, amount) row into the database
 deposit :: Connection -> T.Text -> Int -> IO ()
-deposit = todo
+deposit db account amount = execute db depositQuery (account, amount)
+
 
 ------------------------------------------------------------------------------
 -- Ex 2: Fetching an account's balance. Below you'll find
@@ -109,7 +113,9 @@ balanceQuery :: Query
 balanceQuery = Query (T.pack "SELECT amount FROM events WHERE account = ?;")
 
 balance :: Connection -> T.Text -> IO Int
-balance = todo
+balance db account = do
+   result <- query db balanceQuery [account]
+   return $ sum $ concat result
 
 ------------------------------------------------------------------------------
 -- Ex 3: Now that we have the database part covered, let's think about
@@ -141,14 +147,29 @@ balance = todo
 --   parseCommand [T.pack "deposit", T.pack "madoff", T.pack "123456"]
 --     ==> Just (Deposit "madoff" 123456)
 
-data Command = Deposit T.Text Int | Balance T.Text
-  deriving (Show, Eq)
+data Command = Deposit  T.Text Int 
+             | Withdraw T.Text Int
+             | Balance  T.Text 
+             deriving (Show, Eq)
 
 parseInt :: T.Text -> Maybe Int
 parseInt = readMaybe . T.unpack
 
 parseCommand :: [T.Text] -> Maybe Command
-parseCommand = todo
+parseCommand []                              = Nothing
+parseCommand (t:ts) | t == T.pack "balance"  = parseBalanceCommand  ts 
+                    | t == T.pack "deposit"  = parseDepositCommand  ts 
+                    | t == T.pack "withdraw" = parseWithdrawCommand ts 
+                    | otherwise              = Nothing 
+  where
+    parseBalanceCommand [acc] = Just $ Balance acc
+    parseBalanceCommand    _  = Nothing
+
+    parseDepositCommand  [acc,amt] = parseInt amt >>= \n -> Just $ Deposit acc n
+    parseDepositCommand        _  = Nothing
+
+    parseWithdrawCommand [acc,amt] = parseInt amt >>= \n -> Just $ Withdraw acc n
+    parseWithdrawCommand        _  = Nothing
 
 ------------------------------------------------------------------------------
 -- Ex 4: Running commands. Implement the IO operation perform that takes a
@@ -174,7 +195,10 @@ parseCommand = todo
 --   "0"
 
 perform :: Connection -> Maybe Command -> IO T.Text
-perform = todo
+perform db (Just (Balance  acc))     = T.pack . show <$> balance db acc
+perform db (Just (Deposit  acc amt)) = deposit db acc   amt  >> return (T.pack "OK")
+perform db (Just (Withdraw acc amt)) = deposit db acc (-amt) >> return (T.pack "OK")
+perform  _  Nothing                  = return (T.pack "ERROR")
 
 ------------------------------------------------------------------------------
 -- Ex 5: Next up, let's set up a simple HTTP server. Implement a WAI
@@ -194,7 +218,9 @@ encodeResponse t = LB.fromStrict (encodeUtf8 t)
 -- Remember:
 -- type Application = Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived
 simpleServer :: Application
-simpleServer request respond = todo
+simpleServer request respond = 
+  respond (responseLBS status200 [] ((encodeResponse . T.pack) "BANK"))
+
 
 ------------------------------------------------------------------------------
 -- Ex 6: Now we finally have all the pieces we need to actually
@@ -223,7 +249,10 @@ simpleServer request respond = todo
 -- Remember:
 -- type Application = Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived
 server :: Connection -> Application
-server db request respond = todo
+server db request respond = do
+  msg <- perform db (parseCommand (pathInfo request))
+  respond (responseLBS status200 [] (encodeResponse msg))
+
 
 port :: Int
 port = 3421
